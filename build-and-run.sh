9c441a3b371fc06d73ed15a05757a1d8055fb032
CalixtoNeto <36490064+CalixtoNeto@users.noreply.github.com>
2025-08-16 04:44:01 -0300

    feat: compose for local builds

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Build Maven modules using all CPU cores
-mvn -T 1C clean package -DskipTests
-
-# Build Docker images in parallel for faster builds
-docker build -f Dockerfile.backend -t rinha-backend:latest . &
-docker build -f Dockerfile.loadbalancer -t rinha-loadbalancer:latest . &
-wait
-
-# Subir com Docker Compose usando imagens locais
-cd participantes/calixto-neto/
-docker compose up -d
+# Subir com Docker Compose usando build local
+docker compose up -d --build
 echo "Aplicação buildada e rodando! Acesse via localhost:9999"
